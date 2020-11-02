dbac67abcdb22d558237f109d4baf59532467f55
alioguzhan <aoguzhanyildiz@gmail.com>
2020-11-02 22:53:12 +0300

    formula file fixes

--- Formula/dobby.rb
+++ Formula/dobby.rb
@@ -1,8 +1,7 @@
 class Dobby < Formula
-  desc "Command line program to track and save your work / study activity."
+  desc "Command-line program to track and save your work / study activity"
   homepage "https://github.com/alioguzhan/dobby"
-  url "https://github.com/alioguzhan/dobby/archive/v0.1.zip"
-  version "v0.1"
+  url "https://github.com/alioguzhan/dobby/archive/v0.1.tar.gz"
   sha256 "1230602582b5dd42a674c9763169e6ca620800584ed3724ad6f9a80458b4d160"
   license "MIT"
 
