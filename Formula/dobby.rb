c9d561249ba04c09d6a3720ceb6c935ba1c62410
alioguzhan <aoguzhanyildiz@gmail.com>
2020-11-07 03:02:46 +0300

    add configure step to installation

--- Formula/dobby.rb
+++ Formula/dobby.rb
@@ -1,13 +1,17 @@
 class Dobby < Formula
   desc "Command-line program to track and save your work / study activity"
   homepage "https://github.com/alioguzhan/dobby"
-  url "https://github.com/alioguzhan/dobby/archive/v0.1.tar.gz"
-  sha256 "9282c7a9daa1863ac17bba5413d35730ca10bb1e3d396ffa1802247f189db7f1"
+  url "https://github.com/alioguzhan/dobby/releases/download/v0.1/dobby-0.1.tar.gz"
+  sha256 "8625ed0758b238d6c7cea26cf57cc0d8bb08006a30c64488e8ab307853ee65e8"
   license "MIT"
 
   depends_on "make" => :build
 
   def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     system "make"
     system "make", "install"
   end
